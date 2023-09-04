6d763c6e2938ff305919789f1eea545d53870152
Alexandre Calonne <alexandre.calonne@epitech.digital>
2023-09-04 15:41:32 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,3 +39,19 @@ closeBtn.addEventListener('click', function (event) {
   event.stopPropagation();
   closeNav();
 });
+
+const fadeIns = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+  const isSmallScreen = window.matchMedia("(max-width: 650px)").matches;
+
+  fadeIns.forEach(fadeIn => {
+    if (isSmallScreen && fadeIn.classList.contains('disable-section')) {
+      return;
+    }
+
+    if (fadeIn.getBoundingClientRect().top < window.innerHeight / 1.3) {
+      fadeIn.classList.add('active');
+    }
+  });
+});
